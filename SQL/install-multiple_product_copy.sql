804e53bac0dc0666cec74a4abec69dbbe9c82df0
torvista <torvista@users.noreply.github.com>
2019-11-28 13:14:43 +0100

    correct install sql add uninstall sql

--- SQL/install-multiple_product_copy.sql
+++ SQL/install-multiple_product_copy.sql
@@ -1,2 +1,2 @@
 INSERT INTO `admin_pages` (`page_key`, `language_key`, `main_page`, `page_params`, `menu_key`, `display_on_menu`, `sort_order`) VALUES
-('MultipleProductCopy', 'BOX_CATALOG_MULTI_COPY', 'FILENAME_MULTI_COPY', '', 'catalog', 'Y', 30);
+('multipleProductCopy', 'BOX_CATALOG_MULTI_COPY', 'FILENAME_MULTI_COPY', '', 'catalog', 'Y', 30);
