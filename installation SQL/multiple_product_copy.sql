fd662a07f00f7dc63cd4bc9cc6a1bb0b92322ddf
torvista <torvista@users.noreply.github.com>
2019-11-28 13:07:51 +0100

    correct SQL typo

--- installation SQL/multiple_product_copy.sql
+++ installation SQL/multiple_product_copy.sql
@@ -1,7 +1,2 @@
-#
-# ONLY FOR Zen Cart v1.5
-#
-# $Id: multi_product_copy.php ver 1.391 by Linda McGrath 2011-11-10
-
 INSERT INTO `admin_pages` (`page_key`, `language_key`, `main_page`, `page_params`, `menu_key`, `display_on_menu`, `sort_order`) VALUES
-('MultipleProductCopy', 'BOX_CATALOG_MUTLI_COPY', 'FILENAME_MULTI_COPY', '', 'catalog', 'Y', 30);
+('MultipleProductCopy', 'BOX_CATALOG_MULTI_COPY', 'FILENAME_MULTI_COPY', '', 'catalog', 'Y', 30);
