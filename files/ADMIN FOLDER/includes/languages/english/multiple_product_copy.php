099082baa18d1a296562a413aabddf3252dd8e61
Steve <4391026+torvista@users.noreply.github.com>
2020-10-26 14:05:09 +0100

    shorten DUPLICATE to COPY as per ZC158

--- files/ADMIN FOLDER/includes/languages/english/multiple_product_copy.php
+++ files/ADMIN FOLDER/includes/languages/english/multiple_product_copy.php
@@ -132,7 +132,7 @@ define('TEXT_PRODUCTS_COPIED_TO', '%1$u product(s) copied to Category "%3$s" ID#
 //Confirm Copy Duplicates
 //these four constants used in copy_product_confirm
 if (!defined('TEXT_DUPLICATE_IDENTIFIER')) {
-define('TEXT_DUPLICATE_IDENTIFIER', '[DUPLICATE]');
+define('TEXT_DUPLICATE_IDENTIFIER', '[COPY]');
 }
 define('TEXT_COPY_AS_DUPLICATE_ATTRIBUTES', 'Attributes copied from Product ID#%1$u to duplicate Product ID#%2$u');
 define('TEXT_COPY_AS_DUPLICATE_METATAGS', 'Metatags for Language ID#%1$u copied from Product ID#%2$u to duplicate Product ID#%3$u');
