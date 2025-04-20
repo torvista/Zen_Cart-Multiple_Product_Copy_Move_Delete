5f481609caaedf314974787ae70d29b1203b8791
torvista <4391026+torvista@users.noreply.github.com>
2025-04-20 09:26:20 +0200

    convert to .lang

--- files/ADMIN FOLDER/includes/languages/english/extra_definitions/lang.multiple_product_copy.php
+++ files/ADMIN FOLDER/includes/languages/english/extra_definitions/lang.multiple_product_copy.php
@@ -1,6 +1,12 @@
 <?php
+
+declare(strict_types=1);
+
 // Plugin Multiple Product Copy
 // https://github.com/torvista/Zen_Cart-Multiple_Products_Copy_Move_Delete
 // @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
+$define = [
+    'BOX_CATALOG_MULTIPLE_PRODUCT_COPY' => 'Multiple Product Copy 1',
+];
 
-define('BOX_CATALOG_MULTIPLE_PRODUCT_COPY', 'Multiple Product Copy');
+return $define;
